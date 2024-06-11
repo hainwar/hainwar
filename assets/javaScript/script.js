0911a520a5bfca1cddeedae00941cb9b9ff89bd1
HAIRUL ANWAR <136827134+hainwar@users.noreply.github.com>
2024-06-11 14:32:28 +0000

    .

---
+++ assets/javaScript/script.js
@@ -0,0 +1,9 @@
+// Toggle navigation menu on mobile
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
